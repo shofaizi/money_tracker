26989b9ea486b44e1eedc415bf9cc52d3ae6380b
Sho <sfaizi1@outlook.com>
2016-12-07 11:04:52 -0800

    finalize entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :budgets
+
+  validates :type, presence: true, length: {minimum: 5}
+  validates :period, presence: true
+  validates :amount_cents, presence: true, numericality: true
 end
