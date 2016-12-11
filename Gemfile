c3d785ac8d8efbf8084191a4892a4274a5222a57
Sho <sfaizi1@outlook.com>
2016-12-10 20:30:34 -0800

    implement twitter login

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'bootstrap'
 gem 'chart-js-rails'
 gem 'momentjs-rails', '~> 2.11'
 gem 'tachyons-rails'
+gem 'twitter'
+gem 'omniauth-twitter'
 
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208015140) do
+ActiveRecord::Schema.define(version: 20161210191838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 20161208015140) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
+    t.text     "oauth_raw_data"
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", using: :btree
   end
 
   add_foreign_key "budgets", "users"
