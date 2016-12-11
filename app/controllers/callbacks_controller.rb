fc06a1f45a8f52c9fb80c242f97273402ac7b4e2
Sho <sfaizi1@outlook.com>
2016-12-10 20:33:06 -0800

    new commit

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,16 @@
+class CallbacksController < ApplicationController
+
+  def twitter
+  # step 1. search for a user with the given provider/ uid
+  twitter_data = request.env['oauth.auth']
+  user = User.find_from_oauth(twitter_data)
+
+  # step 2. create the user if the user is not found
+  if user.nil?
+    user ||= User.create_from_oauth(twitter_data)
+  end
+  # step 3. sign in the user
+  session[:user_id] = user.id
+  redirect_to root_path, notice: "Thanks for signing up with Twitter!"
+  end
+end
