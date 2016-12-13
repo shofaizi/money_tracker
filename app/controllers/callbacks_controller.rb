74335c92905dcbf1005306bc8a665636854f5bf3
Sho <sfaizi1@outlook.com>
2016-12-12 16:27:43 -0800

    apply twitter api log in

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,10 +1,10 @@
 class CallbacksController < ApplicationController
+  skip_before_action :authenticate_user
 
   def twitter
   # step 1. search for a user with the given provider/ uid
-  twitter_data = request.env['oauth.auth']
+  twitter_data = request.env['omniauth.auth']
   user = User.find_from_oauth(twitter_data)
-
   # step 2. create the user if the user is not found
   if user.nil?
     user ||= User.create_from_oauth(twitter_data)
