13b953a95ce6460217026412a0c1a899d929d0bc
Sho <sfaizi1@outlook.com>
2016-12-13 16:46:12 -0800

    add jquery to transaction index

--- app/assets/javascripts/transactions.js
+++ app/assets/javascripts/transactions.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('.trash').click(function(){
-    $(this).();
+    var trashId = $(this).attr('data-id');
+    $('[data-id='+trashId+']').fadeOut(1000);
   });
 });
