96fb7900059188291be20038481c381f8bcd22f9
Sho <sfaizi1@outlook.com>
2016-12-15 15:04:55 -0800

    modify button_overview.js

--- app/assets/javascripts/button_overview.js
+++ app/assets/javascripts/button_overview.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
   var getInitialData = function() {
     $.get('http://localhost:3000/overview/ajax?begin=' + new Date(), function(data){
-      console.log(data);
+      console.log("Data from home:", data);
       response_data = data
       $("#income").html(data.income);
       $("#expenses").html(data.expenses);
@@ -13,23 +13,31 @@ $(document).ready(function(){
   }
 
   var setDates = function(data){
-    var buttonLeft = $('.button-left')[0],
-        dateLabelLeft = $('.date-label-left')[0],
-        buttonRight = $('.button-right')[0],
-        dateLabelRight = $('.date-label-right')[0];
+    var buttonLeft = $('.button-left'),
+        dateLabelLeft = $('.date-label-left'),
+        buttonRight = $('.button-right'),
+        dateLabelRight = $('.date-label-right');
 
-    buttonLeft.dataset.date = data.date_beginning;
-    dateLabelLeft.innerHTML = data.date_beginning;
-    buttonRight.dataset.date = data.date_end;
-    dateLabelRight.innerHTML = data.date_end;
+    buttonLeft.attr('data-date', data.date_beginning);
+    dateLabelLeft.html(data.date_beginning);
+    buttonRight.attr('data-date', data.date_end);
+    dateLabelRight.html(data.date_end);
   }
 
   getInitialData()
 
-  $('.button-left').click(function(){
+  $('.button-left, .button-right').click(function(e){
     var begin_date = this.dataset.date;
-    console.log('inside left', this.dataset);
-    $.get('http://localhost:3000/overview/ajax?begin=' + begin_date, function(data){
+
+
+    console.log('Begin data:', begin_date)
+
+    var direction = /right/.test(this.className) ? 'end' : 'begin'
+    var requestUrl = 'http://localhost:3000/overview/ajax?' + direction + '=' + begin_date
+
+    console.log('requestUrl:', requestUrl)
+    $.get(requestUrl, function(data){
+      console.log(data)
       response_data = data
       setDates(data);
       $("#income").html(data.income);
@@ -39,20 +47,20 @@ $(document).ready(function(){
     });
   });
 
-  $('.button-right').click(function(){
-    if(response_data !== undefined) {
-      var end_date = response_data.date_end
-    } else {
-      var end_date = this.dataset.date
-    }
-
-    $.get('http://localhost:3000/overview/ajax?end=' + end_date, function(data){
-      response_data = data
-      setDates(data);
-      $("#income").html(data.income);
-      $("#expenses").html(data.expenses);
-      $("#budget-left").html(data.budget_left);
-      $(".date-header").html(data.date_beginning + ' - ' + data.date_end);
-    });
-  });
+  // $('.button-right').click(function(){
+  //   if(response_data !== undefined) {
+  //     var end_date = response_data.date_end
+  //   } else {
+  //     var end_date = this.dataset.date
+  //   }
+  //
+  //   $.get('http://localhost:3000/overview/ajax?end=' + end_date, function(data){
+  //     response_data = data
+  //     setDates(data);
+  //     $("#income").html(data.income);
+  //     $("#expenses").html(data.expenses);
+  //     $("#budget-left").html(data.budget_left);
+  //     $(".date-header").html(data.date_beginning + ' - ' + data.date_end);
+  //   });
+  // });
 });
